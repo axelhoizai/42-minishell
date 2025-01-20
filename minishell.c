4fd77b5be7710df9f1ab1e949d0a7274a06f69e4
Mickael Demare <mdemare@c1r1p12.42lehavre.fr>
2025-01-20 17:02:11 +0100

    add

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mdemare <mdemare@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 13:16:24 by mdemare           #+#    #+#             */
-/*   Updated: 2025/01/20 13:53:13 by mdemare          ###   ########.fr       */
+/*   Updated: 2025/01/20 16:29:23 by mdemare          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,34 @@ void	handle_sigint(int sig)
 	rl_redisplay();
 }
 
-void	get_user_and_host()
+char	*get_promt()
 {
-	char *username;
-	char hostname[1024];
+	char	*username;
+	char	hostname[1024];
+	char	*prompt =NULL;
+	char	*tmp;
+	char	*tmp2;
 
 	username = getenv("USER");
 	if (!username)
 		username = "unknown";
-
 	if (gethostname(hostname, sizeof(hostname)) != 0)
-		strcpy(hostname, "unknown");
-	printf("%s", username);
-	printf("@");
-	printf("%s >\n", ft_strtok(hostname, "."));
+		ft_strlcpy(hostname, "unknown", ft_strlen("unknown"));
+	username = ft_strjoin(username, "@");
+	tmp = ft_strtok(hostname, ".");
+	tmp2 = ft_strjoin(username, tmp);
+	prompt = ft_strdup(tmp2);
+	free(username);
+	free(tmp2);
+	ft_strtok(NULL, ".");
+	return (prompt);
 }
 
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	char	*prompt;
 	(void)argc;
 	(void)argv;
 	(void)envp;
@@ -48,11 +56,17 @@ int	main(int argc, char **argv, char **envp)
 	signal(SIGINT, handle_sigint);
 	while (1)
 	{
-		get_user_and_host();
-		input = readline("minishell> ");
+		prompt = get_promt();
+		printf("\e[0;32m");
+		input = readline(prompt);
+		printf("\e[m");
+		free(prompt);
 		if (!input) // CTRL+D
 		{
 			printf("Exiting...\n");
+			rl_clear_history();
+			// free(prompt);
+			free(input);
 			break;
 		}
 		if (*input)
